1b093129f54d2ce540947486e534fafd8f478744
David_de_Toro <114107350+DavidDeToro@users.noreply.github.com>
2023-09-13 20:21:32 +0200

    imprimirSuma

--- proyectoJava/Main.java
+++ proyectoJava/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main {
         System.out.print("Hello");
 
+        int num = 1;
+
         for(int i=0; i < 3; i++){
             System.out.println("123");
         }
+
+        System.out.println(num + 1);
     }
 }
