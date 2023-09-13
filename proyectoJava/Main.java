3aaa20d55b5b62957d1c764727188be1ed0e8289
David_de_Toro <114107350+DavidDeToro@users.noreply.github.com>
2023-09-13 20:34:47 +0200

    Revert "cambio codigo"

--- proyectoJava/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main {
-        System.out.print("Hello");
-
-        for(int i=0; i < 3; i++){
-            System.out.println("123");
-        }
-    }
-}
