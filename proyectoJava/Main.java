55ce381f443388c690632c9b1948232071f71e85
David_de_Toro <114107350+DavidDeToro@users.noreply.github.com>
2023-09-13 20:02:08 +0200

    cambio codigo

---
+++ proyectoJava/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main {
+        System.out.print("Hello");
+
+        for(int i=0; i < 3; i++){
+            System.out.println("123");
+        }
+    }
+}
